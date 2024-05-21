d504669cd0242bd65cfc96409aaaddff99565227
Mitchel <mitchel_jager@hotmail.com>
2024-05-21 22:46:55 +0200

    chore: Use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+      paths: {
+        base: dev ? '' : process.env.BASE_PATH,
+      }
+    })
 	}
 };
 
